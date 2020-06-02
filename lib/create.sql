9a5a920fbc2e4df52940a04eaf05388c9c84270b
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-02 14:56:26 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,4 +2,4 @@ CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, fundin
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
-CREATE TABLE pledges (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
