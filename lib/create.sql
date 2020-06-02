2cb9b3ee926bd8a521dd9962b6ef7f9aef967de7
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-02 14:54:01 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date);
+CREATE TABLE projects (id, title, category, funding_goal, start_date, end_date);
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
 
