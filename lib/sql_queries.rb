3abb99d3c8ba097f4898d0612c805efb0f09beb1
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-02 15:04:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,6 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "
   SELECT users.name, users.age, SUM(pledges.amount)
   FROM users JOIN pledges ON users.id = pledges.users_id
+  GROUP BY pledges.id
   GROUP BY users.name
   "
 end
