2c282c5a321d87048ba0afa49583155e08c30b19
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-02 14:51:40 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,16 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+
+INSERT INTO groceries VALUES (1, "Apples", 2, 1);
+INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
+INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+
+INSERT INTO groceries VALUES (1, "Apples", 2, 1);
+INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
+INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
 
 INSERT INTO groceries VALUES (1, "Apples", 2, 1);
 INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
