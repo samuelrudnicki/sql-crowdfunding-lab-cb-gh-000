dc7ba43710d69a9f38b6298940fb8afed6deb82a
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-02 15:09:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "
   SELECT projects.title, SUM(pledges.amount)
   FROM projects JOIN pledges
-  GROUP BY projects.title
   HAVING projects.funding_goal <= SUM(pledges.amount)"
 end
 
