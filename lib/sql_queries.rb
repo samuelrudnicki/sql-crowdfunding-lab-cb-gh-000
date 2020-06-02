907271b0b431dd023cc17317d98546fecbcc3928
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-02 15:00:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   SELECT projects.title, pledges.amount
   FROM projects JOIN pledges ON projects.id = pledges.project_id
   GROUP BY projects.title
-  ORDER BY projects.title DESC
+  ORDER BY projects.title
   "
 end
 
